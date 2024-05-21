cdbe9816a4d25fada63af8e56bbefcd201bffaad
matheusned15 <matheusned15@gmail.com>
2024-05-21 14:50:12 -0300

    First commit

---
+++ producer/src/main/java/com/example/nedtech/kafka/producer/resource/MensagemResource.java
@@ -0,0 +1,21 @@
+package com.example.nedtech.kafka.producer.resource;
+
+
+import com.example.nedtech.kafka.producer.service.MensagemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/mensagem")
+public class MensagemResource {
+
+    @Autowired
+    MensagemService mensagemService;
+
+    @PostMapping
+    public ResponseEntity<String> enviarMensagem(@RequestBody String mensagem){
+        mensagemService.sendMessage(mensagem);
+        return ResponseEntity.ok().body("Mensagem enviada com sucesso: " + mensagem);
+    }
+}
